52de04f60ec7e56202b0d77961c5e88b841cead9
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-12-07 11:35:43 -0500

    tables exercises

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name  VARCHAR(50) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT,
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
+        );
+
+
